2e96e03f4d6209b27f450badaebac87fe26d3ce2
Jericho Nasser <jerichonasser@yahoo.com>
2023-10-23 19:46:32 +0000

    JN

--- src/main/java/WordCountMap.java
+++ src/main/java/WordCountMap.java
@@ -15,7 +15,15 @@ public class WordCountMap {
      * @return a Map (key/value pairs) of all the words mapped to their number of occurrences.
      */
     public Map<String, Integer> returnWordMap(String words){
-
-        return null;
+        Map<String, Integer> wordCounts = new HashMap<>();
+        String[] wordArray = words.split(" ");
+        for (String word : wordArray) {
+            if (wordCounts.containsKey(word)) {
+                wordCounts.put(word, wordCounts.get(word)+1);
+            } else {
+                wordCounts.put(word, 1);
+            }
+        }
+        return wordCounts;
     }
 }
